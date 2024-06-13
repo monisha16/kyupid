79dc640b4ca800b46065e476b6cc9a6a5fd75713
Monisha Tarkar <monishatarkar1625@gmail.com>
2024-06-13 16:24:43 +0530

    add raw data (#10)

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,18 +1,18 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Map from '../Map/Map';
 import Range from '../Range/Range';
 
 const HomePage = () => {
-    const [mapType, setMapType] = useState('pro');
+  const [mapType, setMapType] = useState('pro');
 
-    return ( 
-        <div>
-            <Header mapType={mapType} setMapType={setMapType}/>
-            <Map mapType={mapType} />
-            <Range mapType={mapType} />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Header mapType={mapType} setMapType={setMapType} />
+      <Map mapType={mapType} />
+      <Range mapType={mapType} />
+    </div>
+  );
+};
+
 export default HomePage;
